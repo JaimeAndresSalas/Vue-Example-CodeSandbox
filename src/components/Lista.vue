815a15f1ac7b4f642e3d5f121255cd8ba6371620
Jaime Andres Salas <jsalasmoreira@gmail.com>
2020-04-25 10:17:56 -0500

    Terminado Ejemplo
    Se termina ejemplo de uso de una pequena web, con Vue y Vuex

--- src/components/Lista.vue
+++ src/components/Lista.vue
@@ -2,23 +2,31 @@
   <div>
     <ul class="list-group">
       <li
-        v-for="item of frutas"
+        v-for="(item, index) of arrayOrdenado"
         :key="item.id"
+        @click="aumentar(index)"
         class="list-group-item d-flex justify-content-between align-items-center"
       >
         {{item.nombre}}
         <span class="badge badge-primary badge-pill">{{item.cantidad}}</span>
       </li>
     </ul>
+    <button @click="reset" class="btn btn-danger btn-block">Reiniciar</button>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapMutations } from "vuex";
 export default {
   name: "Lista",
   computed: {
-    ...mapState(["frutas"])
+    ...mapState(["frutas"]),
+    arrayOrdenado() {
+      return this.frutas.sort((a, b) => b.cantidad - a.cantidad);
+    }
+  },
+  methods: {
+    ...mapMutations(["aumentar", "reset"])
   }
 };
 </script>
